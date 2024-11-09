450d76c35d84a20842370b5ef34daa4b34e56166
Shulen Fourcade <julenfourcade98@gmail.com>
2024-11-09 09:45:16 -0300

    Se incluye /interfaces/ en gitignore

--- tp3distribuidos_grupo10/src/main/java/com/stockearte/tp3_grupo10/soap/interfaces/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Este archivo ha sido generado por Eclipse Implementation of JAXB v3.0.0 
-// Visite https://eclipse-ee4j.github.io/jaxb-ri 
-// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2024.11.09 a las 09:22:49 AM ART 
-//
-
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://www.stockearte.com/tp3_grupo10/soap/interfaces", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.stockearte.tp3_grupo10.soap.interfaces;
