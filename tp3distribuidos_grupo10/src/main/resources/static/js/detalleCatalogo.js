419244c7dcbcf476be5805b8a8aed6c720a3f648
daianaVillarreal <daiu.ale.07@gmail.com>
2024-11-10 21:50:30 -0300

    eliminar producto de catalogo

--- tp3distribuidos_grupo10/src/main/resources/static/js/detalleCatalogo.js
+++ tp3distribuidos_grupo10/src/main/resources/static/js/detalleCatalogo.js
@@ -22,38 +22,39 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function fetchCatalogoDetail(idCatalogo) {
-  fetch(`http://localhost:8080/api/catalogo/getById?idCatalogo=${idCatalogo}`)
-    .then(response => response.json())
-    .then(catalogo => {
-      const detailDiv = document.getElementById("catalogo-detail");
-      let catalogoHTML = `
-        <div class="card mb-3">
-          <div class="card-body">
-            <h5 class="card-title">ID Catálogo: ${catalogo.id}</h5>
-            <p class="card-text"><strong>ID Tienda:</strong> ${catalogo.tienda.codigo}</p>
-            <h6 class="mt-3">Productos en el catálogo:</h6>
-            <ul class="list-group">
-      `;
-      if (catalogo.producto && catalogo.producto.length > 0) {
-        catalogo.producto.forEach(producto => {
-          catalogoHTML += `
-            <li class="list-group-item">
-              <p><strong>Código:</strong> ${producto.codigo}</p>
-              <p><strong>Nombre:</strong> ${producto.nombre}</p>
-              <p><strong>Talle:</strong> ${producto.talle}</p>
-              <p><strong>Color:</strong> ${producto.color}</p>
-              ${producto.foto ? `<img src="${producto.foto}" style="max-width: 100px;">` : "<p>Sin foto disponible</p>"}
-            </li>
-          `;
-        });
-      } else {
-        catalogoHTML += "<p>No hay productos en este catálogo.</p>";
-      }
-      catalogoHTML += "</ul></div></div>";
-      detailDiv.innerHTML = catalogoHTML;
-    })
-    .catch(error => console.error("Error al obtener detalles del catálogo:", error));
-}
+	  fetch(`http://localhost:8080/api/catalogo/getById?idCatalogo=${idCatalogo}`)
+	    .then(response => response.json())
+	    .then(catalogo => {
+	      const detailDiv = document.getElementById("catalogo-detail");
+	      let catalogoHTML = `
+	        <div class="card mb-3">
+	          <div class="card-body">
+	            <h5 class="card-title">ID Catálogo: ${catalogo.id}</h5>
+	            <p class="card-text"><strong>ID Tienda:</strong> ${catalogo.tienda.codigo}</p>
+	            <h6 class="mt-3">Productos en el catálogo:</h6>
+	            <ul class="list-group">
+	      `;
+	      if (catalogo.producto && catalogo.producto.length > 0) {
+	        catalogo.producto.forEach(producto => {
+	          catalogoHTML += `
+	            <li class="list-group-item position-relative">
+	              <p><strong>Código:</strong> ${producto.codigo}</p>
+	              <p><strong>Nombre:</strong> ${producto.nombre}</p>
+	              <p><strong>Talle:</strong> ${producto.talle}</p>
+	              <p><strong>Color:</strong> ${producto.color}</p>
+	              ${producto.foto ? `<img src="${producto.foto}" style="max-width: 100px;">` : "<p>Sin foto disponible</p>"}
+	              <button class="btn btn-danger position-absolute top-0 end-0 m-2" onclick="removeProductFromCatalog(${idCatalogo}, ${producto.codigo})">Eliminar</button>
+	            </li>
+	          `;
+	        });
+	      } else {
+	        catalogoHTML += "<p>No hay productos en este catálogo.</p>";
+	      }
+	      catalogoHTML += "</ul></div></div>";
+	      detailDiv.innerHTML = catalogoHTML;
+	    })
+	    .catch(error => console.error("Error al obtener detalles del catálogo:", error));
+	}
 
 function loadProducts() {
   fetch("http://localhost:8080/api/producto/getAll")
@@ -79,11 +80,28 @@ function addProductToCatalog(codigoCatalogo, codigoProducto) {
 	  })
 	  .then(response => {
 	    if (response.ok) {
-	      alert("Producto agregado al catálogo exitosamente");
-	      fetchCatalogoDetail(codigoCatalogo); // Recargar los detalles del catálogo
+	      fetchCatalogoDetail(codigoCatalogo); 
 	    } else {
 	      throw new Error("Error al agregar producto");
 	    }
 	  })
 	  .catch(error => console.error("Error al agregar producto al catálogo:", error));
 	}
+
+
+function removeProductFromCatalog(codigoCatalogo, codigoProducto) {
+  fetch(`http://localhost:8080/api/catalogo/eliminarProducto?codigoCatalogo=${codigoCatalogo}&codigoProducto=${codigoProducto}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  .then(response => {
+    if (response.ok) {
+      fetchCatalogoDetail(codigoCatalogo); 
+    } else {
+      throw new Error("Error al eliminar producto");
+    }
+  })
+  .catch(error => console.error("Error al eliminar producto del catálogo:", error));
+}
